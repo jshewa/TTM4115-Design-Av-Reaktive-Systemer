aa397c25889ab94afcb903ea2e50fc165b620a86
Sindre Fjermestad <sindresfjermestad@gmail.com>
2017-04-10 12:01:23 +0200

    started app in VS with a btn to map with a static marker and a circular area, as a start and skeleton for further dev.

---
+++ Applikasjon/ParentApp/CordovaParentWebApp/www/scripts/map.js
@@ -0,0 +1,9 @@
+﻿(function () {
+	function myMap() {
+		var mapProp = {
+			center: new google.maps.LatLng(63.419499, 10.402077),
+			zoom: 14,
+		};
+		var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+	}
+})();
