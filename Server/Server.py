8780d5eaf2568a3c371e40b7798b2f13884de0a3
jakobsk <Jakobskok@hotmail.com>
2017-04-18 16:21:44 +0200

    Add files via upload

--- Server/Server.py
+++ Server/Server.py
@@ -12,11 +12,14 @@ class Server():
         address = (self.ip,port)
         self.pieSocket.bind(address)
         self.pieSocket.listen(1)
+
         self.appSocket = socket.socket()
         self.port1=port+1
         address2 = (self.ip,self.port1)
         self.appSocket.bind(address2)
         self.appSocket.listen(1)
+
+
 
     def run(self):
         print("Startet listening on " + self.ip + "," + str(self.port))
@@ -26,9 +29,10 @@ class Server():
                 received_string = conn.recv(4096)
                 received_json = json.loads(received_string.decode('utf-8'))
                 print(received_json)
-                #self.pieSocket.sendall(json.dumps(str(self.payload)).encode('utf-8'))
-                #conn.close()
-                # Send json videre til app self.send_payload(received_json)?
+
+
+                conn2,addr2 = self.appSocket.accept()
+                conn2.send(json.dumps(str(received_json)).encode('utf-8'))
 
             except Exception as e:
                 print (e.message)
